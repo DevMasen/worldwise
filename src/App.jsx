6074d2018f9aaeabb79087ec7e8ff8bf59c2e727
DevMasen <mohammadwh400@gmail.com>
2026-02-13 11:56:09 +0330

    E209: feat: setup css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from '../pages/HomePage';
-import Product from '../pages/Product';
-import Pricing from '../pages/Pricing';
-import PageNotFound from '../pages/PageNotFound';
+import HomePage from './pages/HomePage';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './pages/AppLayout';
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 				<Route path="product" element={<Product />} />
 				<Route path="pricing" element={<Pricing />} />
 				<Route path="*" element={<PageNotFound />} />
+				<Route path="app" element={<AppLayout />} />
 			</Routes>
 		</BrowserRouter>
 	);
